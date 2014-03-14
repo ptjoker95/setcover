0ecbd7ba3080ebccc1cd7038721dcc0c6a55908d
ptjoker95 <ptjoker95@gmail.com>
2014-03-14 10:26:52 +0900

    포크된 걸 지웠다가 다시 해봄. 안의 내용은 수정한 내용을 채워져있음.

--- solver.py
+++ solver.py
@@ -24,13 +24,66 @@ def solve_it(input_data):
     # pick add sets one-by-one until all the items are covered
     solution = [0]*set_count
     coverted = set()
+
+    #set의 비용이 적은 순으로 정렬
+    sets = sorted( sets, key=lambda sets:sets.cost, reverse=False )
+
+    #set의 갯수를 셀 변수
+    cancoverset = []
+    # cover count를 셀 리스트
+    itemcovercount = [0]*item_count
     
+    loop_count = 0
+
+    # 다 커버할 때 까지 루프를 돌린다.
+    while len(coverted) <= item_count-1 and loop_count < 100:
+        loop_count += 1
+        # 가장 적게 커버하는 곳을 찾는다.
+        for s in sets:
+            #print "s[2] for cancoverset: ", s[2]
+            if len(s[2]) is not 0:
+                cancoverset += s[2]
+        #print "cancoverset: ", cancoverset
+            
+        for i in range(0,item_count):
+            itemcovercount[i] = cancoverset.count(i)
+        #print "itemcovercount: ", itemcovercount
+ 
+        minindex = itemcovercount.index(min( i for i in itemcovercount if i is not 0 ))
+        #print "minindex: ", minindex
+               
+        # 가장 적게 커버하는 곳을 가지는 소방서 중에 비용이 가장 작은 곳을 선택한다.
+        for s in sets:
+            #print "s[2] for solution: ", s[2]
+            if minindex in s[2]:
+                #print "s.index: ", s.index
+                solution[s.index] = 1
+                coverted |= set(s.items)
+                break
+        #print "solution: ", solution
+        #print "coverted: ", coverted
+        
+        # 중복된 것 지우기
+        for s in sets:
+            for c in coverted:
+                if c in s[2]:
+                    s[2].remove(c)
+        
+        cancoverset = []
+
+        # 다 채워졌으면 루프 종료
+       # if len(coverted) == 9:
+       #     break
+
+    """
     for s in sets:
         solution[s.index] = 1
         coverted |= set(s.items)
         if len(coverted) >= item_count:
             break
-        
+    """        
+    #set이 선택되었는지 확인
+
     # calculate the cost of the solution
     obj = sum([s.cost*solution[s.index] for s in sets])
 
@@ -38,6 +91,7 @@ def solve_it(input_data):
     output_data = str(obj) + ' ' + str(0) + '\n'
     output_data += ' '.join(map(str, solution))
 
+    #print "coverted:", coverted
     return output_data
 
 
